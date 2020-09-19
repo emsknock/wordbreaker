ab2f02d35a186cd9213f199e3d520f7dd2a21138
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-19 21:03:05 +0300

    Unify deliberation with word-count

--- src/features/text-length.ts
+++ src/features/text-length.ts
@@ -1,7 +1,8 @@
 export default (input: string) => ({
 
-	// Definitely not the most performant way to do this, but if performance
-	// is a concern, NodeJS is inherently the wrong decision.
+	// Like with the word-count feature, this definitely isn't the most
+	// performant way to do this, but if performance is a concern, NodeJS
+	// is inherently the wrong decision.
 	withoutSpaces: input.replace(/\s+/g, "").length,
 
 	withSpaces: input.length,
