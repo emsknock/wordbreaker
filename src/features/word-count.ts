29c17baa291f146ab0723ec34725c6686a58a202
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-19 21:00:41 +0300

    Create word-count function

---
+++ src/features/word-count.ts
@@ -0,0 +1,5 @@
+export default (input: string) =>
+	// Like with the text-length feature, this definitely isn't the most
+	// performant way to do this, but if performance is a concern, NodeJS
+	// is fundamentally the wrong decision.
+	input.trim().split(/\s+/g).length;
