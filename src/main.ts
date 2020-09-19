43b70560276173efafac474f40a8ae890e1bd35a
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-19 20:07:22 +0300

    Fix server listening on NaN

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,12 @@ server.get(
 );
 
 server.listen(
-	Number(process.env.PORT) ?? 3000,
+	Number(process.env.PORT ?? 3000),
 	(err) => {
-		if(err) {
+		if (err) {
 			console.error(err.message);
 			process.exit(1);
 		}
+		console.log(`Listening on ${Number(process.env.PORT ?? 3000)}`)
 	}
 );
