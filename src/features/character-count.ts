a7eb2756b9849f79aa1f6f93644db97cf30b2708
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-19 20:57:41 +0300

    Comment clarifications

--- src/features/character-count.ts
+++ src/features/character-count.ts
@@ -1,5 +1,7 @@
 export default (input: string): { [letter: string]: number }[] => {
 
+	// Maps letters to their counts in the input:
+	// A's char count is stored at 0, B's at 1 etc
 	const letterFrequency = Array.from({ length: 26 }, () => 0);
 
 	for(const char of input.toLowerCase()) {
@@ -10,6 +12,7 @@ export default (input: string): { [letter: string]: number }[] => {
 		if (charCode >= 0x61 && charCode <= 0x7a) continue;
 
 		// Index of the letter in the alphabet (A = 0, B = 1 etc)
+		// 0x61 <=> lower case a — rest of the alphabet follows incrementally
 		const letterIndex = charCode - 0x61;
 
 		letterFrequency[letterIndex]++;
@@ -17,6 +20,9 @@ export default (input: string): { [letter: string]: number }[] => {
 	}
 
 	return letterFrequency.map(
+		// As per above, lower case 'a' maps to char code 0x61, so converting
+		// a position in the English alphabet to a character is as simple
+		// as adding the offset 0x61 to it:
 		(count, index) => ({ [String.fromCharCode(index + 0x61)]: count })
 	);
 
