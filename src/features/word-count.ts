07379eef83a4efe371c87a0bac1ad9baa4aedd3a
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-20 17:32:05 +0300

    Fix wrong wordCount for empty string

--- src/features/word-count.ts
+++ src/features/word-count.ts
@@ -1,5 +1,7 @@
 export default (input: string) =>
-// Like with the text-length feature, this definitely isn't the most
-// performant way to do this, but if performance is a concern, NodeJS
-// is fundamentally the wrong decision.
-    input.trim().split(/\s+/g).length;
+    // Like with the text-length feature, this definitely isn't the most
+    // performant way to do this, but if performance is a concern, NodeJS
+    // is fundamentally the wrong decision.
+    input.length > 0
+        ? input.trim().split(/\s+/g).length
+        : 0;
